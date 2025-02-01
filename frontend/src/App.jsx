30228d63edc705a01c0c3913b5d7cf06c5d43a47
Amesh Manthika <166608158+kgamanthika@users.noreply.github.com>
2025-02-01 22:53:34 +0530

    deleting booking page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@
 
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import BookingList from './components/BookingList';
-import BookingPage from './components/BookingPage';
 import EventForm from './components/EventForm';
 import EventList from './components/EventList';
 
@@ -14,7 +13,6 @@ const App = () => {
           <Route path="/" element={<EventList />} />
           <Route path="/bookings" element={<BookingList />} />
           <Route path="/create-event" element={<EventForm />} />
-          <Route path="/book/:eventId" element={<BookingPage />} />
           </Routes>
     </Router>
   );
